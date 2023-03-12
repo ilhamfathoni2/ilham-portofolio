3eea26c00d98cbd5c05e563449d51cbed6fba0c3
ilham.fathoni <ilham.fathoni>
2023-03-13 00:03:30 +0700

    change icon tab browser

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <Head>
         <title>Ilham</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/assets/vector/logoIF.svg" />
       </Head>
       <Navbar />
       <Socemed />
